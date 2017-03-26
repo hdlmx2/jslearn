6fc6f0f9ad2722427464e53f7c64f6c0053b63bf
hdlmx2 <990489713@qq.com>
2017-03-26 21:10:25 +0800

    动画Demo

--- src/jquery/event/demo.js
+++ src/jquery/event/demo.js
@@ -6,5 +6,14 @@ $(function () {
         var $parent = $(this).parents("div.v_show");
         var $v_show = $parent.find("div.v_content_list");
         var $v_content = $parent.find("div.v_content");
+        var v_width = $v_content.width();
+        var len = $v_show.find("li").length;
+        var page_count = Math.ceil(len / i);
+
+        if (!$v_show.is(":animated")) {
+            $v_show.animate({left: "0px"}, "normal");
+        } else {
+            $v_show.animate({left: "-=" + v_width}, "normal");
+        }
     })
 })
